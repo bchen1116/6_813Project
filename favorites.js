b0a43ff827d56cf0758ff43dea85136016cf924e
Reva <rranka@mit.edu>
2018-04-20 04:26:24 -0400

    working formm

---
+++ favorites.js
@@ -0,0 +1,22 @@
+//navbar code for food selection form pop-up
+document.addEventListener('DOMContentLoaded', function() {
+    var modal = document.querySelector(".form-request");
+    var trigger = document.querySelector(".form");
+    var submit = document.querySelector(".submit-form");
+    var closeButton = document.querySelector(".close-button");
+
+    function toggleModal() {
+        modal.classList.toggle("show-modal");
+    }
+
+    function windowOnClick(event) {
+        if (event.target === modal) {
+            toggleModal();
+        }
+    }
+
+    trigger.addEventListener("click", toggleModal);
+    closeButton.addEventListener("click", toggleModal);
+    submit.addEventListener("click", toggleModal);
+    window.addEventListener("click", windowOnClick);
+});
